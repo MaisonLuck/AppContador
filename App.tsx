5abacaae947c87cf521fb0681595423e60f9dc7f
Maison dos Santos Luck <maison45996986@aluno.ba.senac.br>
2023-10-17 10:01:23 -0300

    Primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Contador from "./src/contador";
 
-export default function App() {
+ 
+
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Contador />
     </View>
   );
-}
+};
 
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+export default App;
